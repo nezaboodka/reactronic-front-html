3314ee66274c5876716dc4c47082fafc129c7358
Yury Chetyrko <ychetyrko@gmail.com>
2020-10-03 16:26:36 +0300

    Reactronic 0.20.100. Reactronic-Front 0.20.25.

--- source/TwoWayFocusSync.ts
+++ source/TwoWayFocusSync.ts
@@ -3,20 +3,20 @@
 // Copyright (C) 2019-2020 Yury Chetyrko <ychetyrko@gmail.com>
 // MIT License: https://raw.githubusercontent.com/nezaboodka/reactronic-front-web/master/LICENSE
 
-import { ToggleRef, Ref } from 'reactronic'
+import { BoolRef, Ref } from 'reactronic'
 import { Trigger } from 'reactronic-front'
 
 export function TwoWayFocusSync(
   id: string,
   target: HTMLElement,
-  focusToggleRef: ToggleRef<any>,
+  focusToggleRef: BoolRef<any>,
   setNativeFocus?: (() => void)
 ): void {
   target.focusBinding = focusToggleRef
   if (setNativeFocus === undefined)
     setNativeFocus = () => target.focus()
   Trigger(id, { focusToggleRef }, (e, o, state) => {
-    const v = state.focusToggleRef.value
+    const v = state.focusToggleRef.deref
     const v1 = state.focusToggleRef.value1
     const active = v === state.focusToggleRef.value1 || (
       v instanceof Ref && v1 instanceof Ref && Ref.sameRefs(v, v1))
